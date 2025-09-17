aeddfb1420b8cd8c442485fce1d4f2d0d6baeb33
trubi <rubi.tombe@ynov.com>
2025-09-17 09:16:38 +0200

    tache1

--- initCharacter.go
+++ initCharacter.go
@@ -15,7 +15,6 @@ type Character struct {
 }
 
 func InitCharacter(name string, class string, level int, pointOfLifeMax int, pointOfLifeCurrent int, inv []string) *Character {
-
 	return &Character{
 		Name:               name,
 		Class:              class,
